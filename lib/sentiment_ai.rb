484d25dfe41cc9a69ceffdeeec8051686d6e6458
BlazingRockStorm <rocker.gryphon@gmail.com>
2024-10-31 18:05:05 +0900

    fix bug: model not calling

--- lib/sentiment_ai.rb
+++ lib/sentiment_ai.rb
@@ -1,19 +1,25 @@
 # frozen_string_literal: true
+require 'openai'
+require 'gemini-ai'
 
 class SentimentAI
-  def self.new(provider, token, options)
-    puts 'TO DO: The new function'
+  def initialize(model, token)
+    @ai = case model
+          when :open_ai
+            OpenAI::Client.new(access_token: token)
+          when :gemini_ai_flash, :gemini_ai_pro
+            model_type = model == :gemini_ai_flash ? 'gemini-flash' : 'gemini-pro'
+            Gemini.new(
+              credentials: {
+                service: 'generative-language-api',
+                api_key: token
+              },
+              options: { model: model_type, server_sent_events: true }
+            )
+          else
+            raise ArgumentError
+          end
   end
-
-  # def build_chat(provider, token, options)
-  #   klass = GenAI::Chat.constants.find do |const|
-  #     const.to_s.downcase == provider.to_s.downcase.gsub('_', '')
-  #   end
-
-  #   raise UnsupportedProvider, "Unsupported Chat provider '#{provider}'" unless klass
-
-  #   @chat = GenAI::Chat.const_get(klass).new(provider: provider, token: token, options: options)
-  # end
 
   def self.analyze
     puts 'TO DO: The analyze function'
