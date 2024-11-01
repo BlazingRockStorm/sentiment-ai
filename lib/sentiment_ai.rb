73c1c214ea3cb62a0db836feb129d04b042208b7
Hoang Duc Quan <rocker.gryphon@gmail.com>
2024-11-01 15:52:35 +0700

    Rewrite as Module (#4)
    * rewrite as module
    
    * rewrite the sentiment_ai as a module
    
    * rubocop fix
    
    * add file to gemspec

--- lib/sentiment_ai.rb
+++ lib/sentiment_ai.rb
@@ -1,27 +1,37 @@
 # frozen_string_literal: true
+
+require 'sentiment_ai/version'
 require 'openai'
 require 'gemini-ai'
 
-class SentimentAI
-  def initialize(model, token)
-    @ai = case model
-          when :open_ai
-            OpenAI::Client.new(access_token: token)
-          when :gemini_ai_flash, :gemini_ai_pro
-            model_type = model == :gemini_ai_flash ? 'gemini-flash' : 'gemini-pro'
-            Gemini.new(
-              credentials: {
-                service: 'generative-language-api',
-                api_key: token
-              },
-              options: { model: model_type, server_sent_events: true }
-            )
-          else
-            raise ArgumentError
-          end
+module SentimentAI
+  def self.adapter=(klass)
+    @klass = klass.capitalize
   end
 
-  def self.analyze
-    puts 'TO DO: The analyze function'
+  def self.new(arg1, arg2)
+    const_get(@klass).new arg1, arg2
+  end
+
+  class Base
+    def initialize(model, token)
+      @generative_ai = case model
+                       when :open_ai
+                         OpenAI::Client.new(access_token: token)
+                       when :gemini_ai_flash, :gemini_ai_pro
+                         model_type = model == :gemini_ai_flash ? 'gemini-flash' : 'gemini-pro'
+                         Gemini.new(
+                           credentials: {
+                             service: 'generative-language-api',
+                             api_key: token
+                           },
+                           options: { model: model_type, server_sent_events: true }
+                         )
+                       else
+                         raise ArgumentError
+                       end
+    end
   end
 end
+
+SentimentAI.adapter = :base
