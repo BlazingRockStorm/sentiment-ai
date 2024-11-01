0049bb217c3ecff2cf00d7097d5bc0f7f770f85a
BlazingRockStorm <rocker.gryphon@gmail.com>
2024-11-01 17:33:44 +0900

    rubocop fix

--- lib/sentiment_ai.rb
+++ lib/sentiment_ai.rb
@@ -5,31 +5,31 @@ require 'openai'
 require 'gemini-ai'
 
 module SentimentAI
-  def self.adapter= klass
+  def self.adapter=(klass)
     @klass = klass.capitalize
   end
 
-  def self.new arg1,arg2
-    const_get( @klass ).new arg1,arg2
+  def self.new(arg1, arg2)
+    const_get(@klass).new arg1, arg2
   end
 
   class Base
     def initialize(model, token)
       @generative_ai = case model
-            when :open_ai
-              OpenAI::Client.new(access_token: token)
-            when :gemini_ai_flash, :gemini_ai_pro
-              model_type = model == :gemini_ai_flash ? 'gemini-flash' : 'gemini-pro'
-              Gemini.new(
-                credentials: {
-                  service: 'generative-language-api',
-                  api_key: token
-                },
-                options: { model: model_type, server_sent_events: true }
-              )
-            else
-              raise ArgumentError
-            end
+                       when :open_ai
+                         OpenAI::Client.new(access_token: token)
+                       when :gemini_ai_flash, :gemini_ai_pro
+                         model_type = model == :gemini_ai_flash ? 'gemini-flash' : 'gemini-pro'
+                         Gemini.new(
+                           credentials: {
+                             service: 'generative-language-api',
+                             api_key: token
+                           },
+                           options: { model: model_type, server_sent_events: true }
+                         )
+                       else
+                         raise ArgumentError
+                       end
     end
   end
 end
