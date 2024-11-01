15f7c0468756e99f35e94708cde325c1f0817c2f
BlazingRockStorm <rocker.gryphon@gmail.com>
2024-11-01 17:13:53 +0900

    rewrite as module

--- lib/sentiment_ai.rb
+++ lib/sentiment_ai.rb
@@ -1,27 +1,29 @@
 # frozen_string_literal: true
-require 'openai'
-require 'gemini-ai'
 
-class SentimentAI
-  def initialize(model, token)
-    @ai = case model
-          when :open_ai
-            OpenAI::Client.new(access_token: token)
-          when :gemini_ai_flash, :gemini_ai_pro
-            model_type = model == :gemini_ai_flash ? 'gemini-flash' : 'gemini-pro'
-            Gemini.new(
-              credentials: {
-                service: 'generative-language-api',
-                api_key: token
-              },
-              options: { model: model_type, server_sent_events: true }
-            )
-          else
-            raise ArgumentError
-          end
-  end
+require 'sentiment_ai/version'
+# require 'openai'
+# require 'gemini-ai'
 
-  def self.analyze
-    puts 'TO DO: The analyze function'
-  end
-end
+# class SentimentAI
+#   def initialize(model, token)
+#     @ai = case model
+#           when :open_ai
+#             OpenAI::Client.new(access_token: token)
+#           when :gemini_ai_flash, :gemini_ai_pro
+#             model_type = model == :gemini_ai_flash ? 'gemini-flash' : 'gemini-pro'
+#             Gemini.new(
+#               credentials: {
+#                 service: 'generative-language-api',
+#                 api_key: token
+#               },
+#               options: { model: model_type, server_sent_events: true }
+#             )
+#           else
+#             raise ArgumentError
+#           end
+#   end
+
+#   def self.analyze
+#     puts 'TO DO: The analyze function'
+#   end
+# end
